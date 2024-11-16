08c64ca33a11c0e428725873c82840d11b472c02
Cleiton Mendes <cleitonmendesgama@gmail.com>
2024-11-16 00:51:15 -0300

    Estrutura do projeto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <title>Cadastro</title>
+</head>
+<body>
+    <form action="" method="">
+        <input type="text" placeholder="Nome do Produto">
+        <input type="text" placeholder="Categoria">
+        <input type="text" placeholder="Preço">
+        <input type="text" placeholder="Estoque">
+        <button>Cadastrar</button>
+    </form>
+</body>
+</html>
